33b8d891f45462a5a4f433574df2f248ca11f11d
gleb226 <gmedvede226@gmail.com>
2023-04-22 14:11:39 +0300

    fixed errors

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 result = []
-
-
 def divider(a, b):
+    if b == 0:
+        raise ZeroDivisionError
     if a < b:
         raise ValueError
     if b > 100:
         raise IndexError
     return a / b
 
+data = {10: 2, 2: 5, 18: 0, 8: 4}
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
 for key in data:
-    res = divider(key, data[kem])
-    result.append(res)
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except (ZeroDivisionError, ValueError, IndexError) as e:
+        print("Виняток типу:", type(e).__name__)
 
 print(result)
